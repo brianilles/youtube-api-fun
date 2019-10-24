ce5d92df9e0de7a201401a5fa45192f1d53afd20
Brian Illes <brianrilles@gmail.com>
2019-10-24 15:32:55 -0700

    Adds channel analytics page linking

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-import "../sass/Home.scss";
+import React from "react";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import styled from "styled-components";
 import { Search } from "styled-icons/boxicons-regular/Search";
-import { Youtube } from "styled-icons/boxicons-logos/Youtube";
+import "../sass/Home.scss";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 
 const InputSearch = styled(Search)`
   color: #000;
@@ -12,13 +13,6 @@ const InputSearch = styled(Search)`
   width: 22px;
   position: absolute;
   margin: 14.5px;
-`;
-
-const SmallYoutube = styled(Youtube)`
-  color: #ff0000;
-  height: 22px;
-  width: 22px;
-  margin-left: 12.5px;
 `;
 
 class Home extends React.Component {
@@ -33,12 +27,14 @@ class Home extends React.Component {
     this.setState({
       [e.target.name]: e.target.value
     });
-    // this.handleSearch(e);
+  };
+
+  handleRouteChange = (e, r) => {
+    console.log(r);
   };
 
   handleSearch = async e => {
     e.preventDefault();
-    console.log("searching...");
     try {
       const endpoint = `http://localhost:8100/api/youtube/search/channel/${this.state.search}`;
       this.setState({ fetchingResults: true });
@@ -59,61 +55,70 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div className="home" onClick={this.clearSearch}>
-        <div className="search">
-          <div className="focus">
-            <h1>A name</h1>
-          </div>
-          <form onSubmit={this.handleSearch}>
-            <div className="search-input-wrapper">
-              <InputSearch />
-              <input
-                type="text"
-                name="search"
-                autoComplete="off"
-                placeholder="Search by username"
-                value={this.state.search}
-                onChange={this.handleChange}
-                className={this.state.results && "active"}
-              />
-              <button>Search</button>
-            </div>
-          </form>
-          {this.state.fetchingResults ||
-            (this.state.results && (
-              <div className="results-wrapper">
-                <div className="results">
-                  {this.state.results ? (
-                    this.state.results.map((r, i) => (
-                      <div className="result" key={i}>
+      <>
+        <Route exact path={`/`}>
+          <div className="home" onClick={this.clearSearch}>
+            <div className="search">
+              <div className="focus">
+                <h1>A name</h1>
+              </div>
+              <form onSubmit={this.handleSearch}>
+                <div className="search-input-wrapper">
+                  <InputSearch />
+                  <input
+                    type="text"
+                    name="search"
+                    autoComplete="off"
+                    placeholder="Search by username"
+                    value={this.state.search}
+                    onChange={this.handleChange}
+                    className={this.state.results && "active"}
+                  />
+                  <button>
+                    {this.state.fetchingResults ? (
+                      <Loader
+                        type="ThreeDots"
+                        color="#fff"
+                        height={30}
+                        width={30}
+                      />
+                    ) : (
+                      "Search"
+                    )}
+                  </button>
+                </div>
+              </form>
+              {this.state.results && (
+                <div className="results-wrapper">
+                  <div className="results">
+                    {this.state.results.map((r, i) => (
+                      <Link
+                        to={`/youtube/channel/${r.snippet.channelId}`}
+                        className="result"
+                        key={i}
+                        onClick={this.handleRouteChange}
+                      >
                         <div className="top">
                           <img
                             src={r.snippet.thumbnails.default.url}
                             alt={r.snippet.channelTitle}
                           />
-                          <p>
-                            {r.snippet.channelTitle}
-                            {/* <span>{r.snippet.description}</span> */}
-                          </p>
+                          <span>{r.snippet.channelTitle}</span>
                         </div>
-                      </div>
-                    ))
-                  ) : (
-                    <p>fetching results</p>
-                  )}
+                      </Link>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))}
-        </div>
-        {/* <Switch>
-          <Route path={`${this.props.match.path}/:`}>
-            <p>hello</p>
+              )}
+            </div>
+          </div>
+        </Route>
+        <Switch>
+          <Route path={`/youtube/channel/`}>
+            <p>hello from channel analytics page!</p>
           </Route>
-          <Route path={match.path}>
-            <h3>Please select a topic.</h3>
-          </Route>
-        </Switch> */}
-      </div>
+        </Switch>
+      </>
     );
   }
 }
